ddb30b145d4dc8e8c1f1a120b7313f631fcee05b
Your NameAlejandro <andresrojas72727264@gmail.com>
2024-06-18 18:56:11 -0500

    Primer acercamiento al Profile

--- storyTrade/src/components/main.jsx
+++ storyTrade/src/components/main.jsx
@@ -5,15 +5,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 // import { TabNavigator } from "./tabNavigator";
 import LoginScreen from "../screens/login";
+import UserProfile from "../screens/profile";
 
 const Stack = createStackNavigator();
 
 export function Main() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen}/>
+      <Stack.Navigator initialRouteName="UserProfile">
+        {/*<Stack.Screen name="Login" component={LoginScreen}/>*/}
         {/* <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }}/> */}
+        <Stack.Screen name="UserProfile" component={UserProfile}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
