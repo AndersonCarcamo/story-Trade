fab1aeab80cb26ba3027a4ea701748dde8826e90
Your NameAlejandro <andresrojas72727264@gmail.com>
2024-06-19 19:15:57 -0500

    primer avanze en addBook

--- storyTrade/src/components/main.jsx
+++ storyTrade/src/components/main.jsx
@@ -5,15 +5,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 // import { TabNavigator } from "./tabNavigator";
 import LoginScreen from "../screens/login";
+import AddBook from '../screens/addBook';
 
 const Stack = createStackNavigator();
 
 export function Main() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen}/>
+      <Stack.Navigator initialRouteName="Add Book">
+        {/* <Stack.Screen name="Login" component={LoginScreen}/> */}
         {/* <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }}/> */}
+        <Stack.Screen name="Add Book" component={AddBook}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
