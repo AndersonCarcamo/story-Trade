f1946bec43036d3c9adac95399570f4282d55dcf
andersoncarcamo <anderson.carcamo@utec.edu.pe>
2024-07-05 17:33:57 -0500

    funciona el cambioContrasenha

--- storyTrade/src/screens/settings.jsx
+++ storyTrade/src/screens/settings.jsx
@@ -1,107 +1,141 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, Alert, Image, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
-import { launchImageLibrary } from 'react-native-image-picker';
 import axios from 'axios';
+
+import Header from '../components/headerH';
 
 const Settings = ({ navigate }) => {
   const navigation = useNavigation();
-  const [imageUrl, setImageUrl] = useState(null);
-  const [customFileName, setCustomFileName] = useState('');
-  const [uploadPath, setUploadPath] = useState('images');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userId, setUserId] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const email = await AsyncStorage.getItem('userEmail');
+      const id = await AsyncStorage.getItem('userToken');
+      setUserEmail(email);
+      setUserId(id);
+    };
+
+    fetchUserData();
+  }, []);
 
   const handleLogout = async () => {
     await AsyncStorage.removeItem('userToken');
+    await AsyncStorage.removeItem('userEmail');
     navigation.replace('Login');
   };
 
-  const uploadFile = async (mediaType) => {
-    const options = {
-      mediaType: mediaType === 'photo' ? 'photo' : 'video',
-      includeBase64: true,
-    };
+  const handleChangePassword = async () => {
+    setErrorMessage(''); // Clear previous error messages
+    if (newPassword !== confirmNewPassword) {
+      setErrorMessage('Las contraseñas nuevas no coinciden');
+      return;
+    }
 
-    launchImageLibrary(options, async (response) => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.assets && response.assets.length > 0) {
-        const file = response.assets[0];
-        const fileExtension = file.type ? file.type.split('/')[1] : 'jpg';
-        const fileName = customFileName ? `${uploadPath}/${customFileName}.${fileExtension}` : `${uploadPath}/${file.fileName}`;
-
-        try {
-          const url = `https://opqwurrut9.execute-api.us-east-2.amazonaws.com/dev/upload`;
-          const uploadResponse = await axios.post(url, {
-            fileName: fileName,
-            fileContent: file.base64,
-          }, {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-
-          Alert.alert('Éxito', `Archivo subido con éxito: ${uploadResponse.data.message}`);
-        } catch (error) {
-          console.log('Error al subir el archivo:', error);
-          Alert.alert('Error', 'No se pudo subir el archivo');
-        }
-      }
-    });
-  };
-
-
-  const fetchImage = async () => {
     try {
-      console.log('entra a fetchear');
-      const response = await axios.get('https://opqwurrut9.execute-api.us-east-2.amazonaws.com/dev/get', {
-        params: {
-          fileName: 'images/book12.jpg',
-        },
+      console.log(userEmail)
+      console.log(currentPassword)
+      const response = await axios.post('https://dbstorytrada-b5fcff8487d7.herokuapp.com/login', {
+        email: userEmail,
+        password: currentPassword,
       });
-      
-      console.log(response);
-      if (response.data) {
-        const imageUrl = `data:${response.headers['content-type']};base64,${response.data}`;
-        setImageUrl(imageUrl);
-        Alert.alert('Éxito', 'Imagen obtenida con éxito');
+
+      if (response.status === 200) {
+        await axios.put(`https://dbstorytrada-b5fcff8487d7.herokuapp.com/users/${userId}`, {
+          password: newPassword,
+        });
+        Alert.alert('Success', 'Password changed successfully');
+        handleModalClose();
       } else {
-        Alert.alert('Error', 'No se pudo obtener la imagen');
+        setErrorMessage('Contraseña actual incorrecta');
       }
     } catch (error) {
-      console.error('Error al obtener la URL de la imagen:', error);
-      Alert.alert('Error', 'No se pudo obtener la URL de la imagen');
+      if (error.response && error.response.status === 401) {
+        setErrorMessage('Contraseña actual incorrecta');
+      } else {
+        console.error(error);
+        setErrorMessage('Ocurrió un error al cambiar la contraseña');
+      }
     }
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+    setCurrentPassword('');
+    setNewPassword('');
+    setConfirmNewPassword('');
+    setErrorMessage('');
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter custom file name"
-        value={customFileName}
-        onChangeText={setCustomFileName}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter upload path (images, videos, avatars)"
-        value={uploadPath}
-        onChangeText={setUploadPath}
-      />
-      <Button title="Edit Profile" onPress={() => navigation.navigate('Complete Profile')} />
-      <Button title="Change Password" onPress={() => navigation.navigate('ChangePassword')} />
-      <Button title="Logout" onPress={handleLogout} color="red" />
-      <Button title="Subir Imagen" onPress={() => uploadFile('photo')} />
-      <Button title="Subir Video" onPress={() => uploadFile('video')} />
-      <Button title="Obtener Imagen" onPress={fetchImage} />
-      {imageUrl && (
-        <Image
-          source={{ uri: imageUrl }}
-          style={styles.image}
-        />
-      )}
+      <Header/>
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Complete Profile')}>
+          <Text style={styles.buttonText}>Editar Perfil</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => setModalVisible(true)}>
+          <Text style={styles.buttonText}>Cambiar Contraseña</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.logoutButton]} onPress={handleLogout}>
+          <Text style={[styles.buttonText, styles.logoutButtonText]}>Cerrar Sesión</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={handleModalClose}
+      >
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Cambiar Contraseña</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Contraseña Actual"
+            secureTextEntry
+            value={currentPassword}
+            onChangeText={setCurrentPassword}
+          />
+          {errorMessage === 'Contraseña actual incorrecta' && (
+            <Text style={styles.errorText}>{errorMessage}</Text>
+          )}
+          <TextInput
+            style={styles.input}
+            placeholder="Nueva Contraseña"
+            secureTextEntry
+            value={newPassword}
+            onChangeText={setNewPassword}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Confirmar Nueva Contraseña"
+            secureTextEntry
+            value={confirmNewPassword}
+            onChangeText={setConfirmNewPassword}
+          />
+          {errorMessage && errorMessage !== 'Contraseña actual incorrecta' && (
+            <Text style={styles.errorText}>{errorMessage}</Text>
+          )}
+          <TouchableOpacity style={styles.modalButton} onPress={handleChangePassword}>
+            <Text style={styles.modalButtonText}>Cambiar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.modalButton, styles.closeButton]}
+            onPress={handleModalClose}
+          >
+            <Text style={styles.modalButtonText}>Cerrar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -109,8 +143,54 @@ const Settings = ({ navigate }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFF7E1', // Background color matching the image's style
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#FFAF65', // Button color matching the image's style
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
+  logoutButton: {
+    backgroundColor: '#FF6B6B', // Red color for logout button
+  },
+  logoutButtonText: {
+    color: '#FFFFFF',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    minHeight: 300, // Ensures minimum height to prevent shifting
+    justifyContent: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   input: {
     height: 40,
@@ -120,10 +200,23 @@ const styles = StyleSheet.create({
     width: '80%',
     paddingHorizontal: 10,
   },
-  image: {
-    width: 200,
-    height: 200,
-    marginTop: 20,
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+  },
+  modalButton: {
+    backgroundColor: '#FFAF65',
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 5,
+    width: '60%',
+    alignItems: 'center',
+  },
+  modalButtonText: {
+    color: '#FFFFFF',
+  },
+  closeButton: {
+    backgroundColor: '#FF6B6B',
   },
 });
 
